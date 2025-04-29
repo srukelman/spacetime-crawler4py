8d4e23e8f803158fea977a03fe7abc751ba31139
srukelman <seanrkelman@gmail.com>
2025-04-28 19:06:39 -0700

    latest

--- crawler/worker.py
+++ crawler/worker.py
@@ -4,6 +4,8 @@ from inspect import getsource
 from utils.download import download
 from utils import get_logger, get_urlhash
 from urllib.parse import urlparse
+import json
+import os
 import scraper
 import time
 
@@ -17,28 +19,39 @@ class Worker(Thread):
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         self.subdomains = {}
+        self.subdomains_file = "subdomains-test.json"
+        if not os._exists(self.subdomains_file):
+            with open(self.subdomains_file, 'w') as f:
+                json.dump({}, f)
+        else:
+            with open(self.subdomains_file, 'r') as f:
+                self.subdomains = json.load(f)
         super().__init__(daemon=True)
         
     def run(self):
         max_page_length = 0
         max_page_url = ''
-        while True:
-            tbd_url = self.frontier.get_tbd_url()
-            if not tbd_url:
-                self.logger.info("Frontier is empty. Stopping Crawler.")
-                break
-            resp = download(tbd_url, self.config, self.logger)
-            self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
-            scraped_urls, length = scraper.scraper(tbd_url, resp)
-            if length > max_page_length:
-                max_page_url = tbd_url
-                max_page_length = length
-            subdomain = urlparse(tbd_url).netloc
-            self.subdomains[subdomain] = 1 + self.subdomains.get(subdomain, 0)
-            for scraped_url in scraped_urls:
-                self.frontier.add_url(scraped_url)
-            self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+        try:
+            while True:
+                tbd_url = self.frontier.get_tbd_url()
+                if not tbd_url:
+                    self.logger.info("Frontier is empty. Stopping Crawler.")
+                    break
+                resp = download(tbd_url, self.config, self.logger)
+                self.logger.info(
+                    f"Downloaded {tbd_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
+                scraped_urls, length = scraper.scraper(tbd_url, resp)
+                if length > max_page_length:
+                    max_page_url = tbd_url
+                    max_page_length = length
+                subdomain = urlparse(tbd_url).netloc
+                self.subdomains[subdomain] = 1 + self.subdomains.get(subdomain, 0)
+                for scraped_url in scraped_urls:
+                    self.frontier.add_url(scraped_url)
+                self.frontier.mark_url_complete(tbd_url)
+                time.sleep(self.config.time_delay)
+        finally:
+            with open(self.subdomains_file, 'w') as f:
+                json.dump(self.subdomains, f)
         print(f'Longest Page: {max_page_url} with {max_page_length}')
--- word_stats.py
+++ word_stats.py
@@ -4,7 +4,8 @@ def fifty_most_common_words(tokens):
     heap = [(-tokens[token], token) for token in tokens]
     res = []
     for _ in range(50):
-        res.append(heapq.heappop(heap)[1])
+        curr = heapq.heappop(heap)
+        res.append(f'{curr[1]}, {-curr[0]}')
     
     with open("fifty_most_common.txt", 'w') as f:
         f.write('\n'.join(res))
@@ -13,3 +14,6 @@ def fifty_most_common_words(tokens):
 def main():
     tokens = json.load(open('tokens.json'))
     fifty_most_common_words(tokens)
+
+
+main()
